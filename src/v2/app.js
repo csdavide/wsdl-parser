e67f8dfccaead260c6b81a014e0c562fc7b48b7d
csdavide <6861768+csdavide@users.noreply.github.com>
2025-10-02 09:13:45 +0200

    Delete src directory

--- src/v2/app.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const { fork } = require('child_process');
-
-const server1Path = path.join(__dirname, 'server1.js');
-const server1Process = fork(server1Path);
-
-const server2Path = path.join(__dirname, 'server2.js');
-const server2Process = fork(server2Path);
